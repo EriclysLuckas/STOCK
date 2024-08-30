18733e40e6f175df9b4f481c22b0f5ecc6de79a8
Ericlys <ericlys_luckas@hotmail.com>
2024-08-29 21:48:19 -0300

    at

--- stockProducts/api/json-server.js
+++ stockProducts/api/json-server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Substitua 'db.json' pelo caminho do seu arquivo JSON
+const router = jsonServer.router('db.json'); // Certifique-se de que o arquivo db.json está na raiz do projeto
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
